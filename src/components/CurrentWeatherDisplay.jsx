5a0355fbd7553d7e3ef734af5806162ab285b325
NTHDang <fang.nguyen72@gmail.com>
2025-08-01 21:44:21 +0700

    12

--- src/components/CurrentWeatherDisplay.jsx
+++ src/components/CurrentWeatherDisplay.jsx
@@ -7,7 +7,10 @@ const CurrentWeatherDisplay = ({ data }) => {
   // Nếu không có dữ liệu, hiển thị thông báo yêu cầu nhập thành phố
   if (!data) {
     return (
-      <p className="text-base mt-5" style={{ color: 'var(--color-text-primary)' }}>
+      <p
+        className="text-base mt-5"
+        style={{ color: "var(--color-text-primary)" }}
+      >
         Enter a city to get weather data.
       </p>
     );
@@ -17,11 +20,16 @@ const CurrentWeatherDisplay = ({ data }) => {
   return (
     <>
       <img src={data.icon} alt="Weather Icon" className="w-[75px] mt-[20px]" />
-      <p className="text-[45px]" style={{ color: 'var(--color-text-primary)' }}>
+      <p className="text-[45px]" style={{ color: "var(--color-text-primary)" }}>
         {data.temperature}°c
       </p>
-      <p className="text-[20px]" style={{ color: 'var(--color-text-primary)' }}>{data.location}</p>
-      <div className="w-full mt-[50px] flex justify-around" style={{ color: 'var(--color-text-primary)' }}>
+      <p className="text-[20px]" style={{ color: "var(--color-text-primary)" }}>
+        {data.location}
+      </p>
+      <div
+        className="w-full mt-[50px] flex justify-around"
+        style={{ color: "var(--color-text-primary)" }}
+      >
         {/* độ ẩm */}
         <div className="flex items-start gap-1.5 text-[20px]">
           <img
@@ -31,7 +39,7 @@ const CurrentWeatherDisplay = ({ data }) => {
           />
           <div>
             <p>{data.humidity}%</p>
-            <span className="block text-[15px]">Humidity</span>
+            <span className="block text-[15px]">Độ ẩm</span>
           </div>
         </div>
         {/* tốc độ gió */}
@@ -39,21 +47,21 @@ const CurrentWeatherDisplay = ({ data }) => {
           <img src={wind_icon} alt="Wind Icon" className="w-[20px] mt-[5px]" />
           <div>
             <p>{data.windSpeed} Km/h</p>
-            <span className="block text-[15px]">Wind Speed</span>
+            <span className="block text-[15px]">Tốc độ gió</span>
           </div>
         </div>
         {/* áp suất */}
         <div className="flex items-start gap-1.5 text-[20px]">
           <div>
             <p>{data.pressure} hPa</p>
-            <span className="block text-[15px]">Pressure</span>
+            <span className="block text-[15px]">Áp suất</span>
           </div>
         </div>
         {/* tầm nhìn */}
         <div className="flex items-start gap-1.5 text-[20px]">
           <div>
             <p>{data.visibility / 1000} km</p>
-            <span className="block text-[15px]">Visibility</span>
+            <span className="block text-[15px]">Tầm nhìn xa</span>
           </div>
         </div>
       </div>
--- src/components/ForecastDisplay.jsx
+++ src/components/ForecastDisplay.jsx
@@ -15,12 +15,10 @@ const ForecastDisplay = ({ forecastData }) => {
       className="w-full mt-30"
       style={{ color: "var(--color-text-primary)" }}
     >
-      <h3
-        className="text-4xl font-bold mb-5 flex items-center justify-center text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 drop-shadow-lg"
-      >
+      <h3 className="text-4xl font-bold mb-5 flex items-center justify-center text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 drop-shadow-lg">
         Dự báo 5 ngày tới
       </h3>
-      <div className="flex flex-wrap justify-center gap-4 mt-10">
+      <div className="flex flex-wrap justify-center gap-4 mt-10 text-xl">
         {groupedByTimeOfDay.map((dayData) => (
           <div
             key={dayData.date}
@@ -37,14 +35,14 @@ const ForecastDisplay = ({ forecastData }) => {
             {Object.entries(dayData.times).map(([timeOfDay, item], index) => (
               <React.Fragment key={timeOfDay}>
                 <div className="text-center mb-2 pb-2">
-                  <p className="text-sm font-bold">{timeOfDay}</p>
+                  <p className="text-lg font-bold">{timeOfDay}</p>
                   <img
                     src={`https://openweathermap.org/img/wn/${item.weather[0].icon}.png`}
                     alt={item.weather[0].description}
-                    className="w-10 h-10 mx-auto"
+                    className="w-15 h-15 mx-auto"
                   />
-                  <p className="text-sm">{Math.floor(item.main.temp)}°c</p>
-                  <p className="text-xs">{item.weather[0].description}</p>
+                  <p className="text-lg">{Math.floor(item.main.temp)}°c</p>
+                  <p className="text-lg">{item.weather[0].description}</p>
                 </div>
                 {index < Object.keys(dayData.times).length - 1 && (
                   <div
